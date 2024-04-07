acbe224df496700bd4fcd52d48db240d2a43b911
bilfri <74826234+bilfri@users.noreply.github.com>
2024-04-07 17:13:31 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/',
+        source: '/about',
         destination: 'https://www.noedbremsen.dk/',
         permanent: true,
       },
